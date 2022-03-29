2e0ebc211bfef30ef879d13a4ea198cf058e7138
chenlx2 <chenlx2@wedoctor.com>
2022-03-30 00:13:51 +0800

    demo

---
+++ src/main/java/com/work/leetcode/binarytree/BinaryTree889.java
@@ -0,0 +1,30 @@
+package com.work.leetcode.binarytree;
+
+//给定两个整数数组，preorder 和 postorder ，其中 preorder 是一个具有 无重复 值的二叉树的前序遍历，postorder 是同一棵
+//树的后序遍历，重构并返回二叉树。
+//
+// 如果存在多个答案，您可以返回其中 任何 一个。
+//
+// 示例 1：
+//输入：preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+//输出：[1,2,3,4,5,6,7]
+//
+// 示例 2:
+//输入: preorder = [1], postorder = [1]
+//输出: [1]
+//
+// 提示：
+// 1 <= preorder.length <= 30
+// 1 <= preorder[i] <= preorder.length
+// preorder 中所有值都 不同
+// postorder.length == preorder.length
+// 1 <= postorder[i] <= postorder.length
+// postorder 中所有值都 不同
+// 保证 preorder 和 postorder 是同一棵二叉树的前序遍历和后序遍历
+
+
+public class BinaryTree889 {
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        return null;
+    }
+}
